63edb2e01b51b7eb14f044012a7f7f808510462f
Kenneth Jimmy <kenjimmy17@gmail.com>
2022-04-08 10:42:12 +0100

    ✅ Implement displaying results on page - Used axios for ajax request - Logged the link, title, and description of the result

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
 new Vue({
   el: "#app",
-  data: {},
+  data: {
+    baseUrl: "https://api.rss2json.com/v1/api.json", // update this before submitting TODO:
+    rssUrl: null,
+    altImg:
+      "https://fliplet.com/wp-content/uploads/sites/4/2018/10/white-400px.png",
+    items: [],
+    isFetchingData: false,
+  },
   methods: {
-    test() {
-      console.log("connected");
+    async handleSearch() {
+      try {
+        this.isFetchingData = true;
+
+        const completUrl = `${this.baseUrl}?rss_url=${this.rssUrl}`;
+        const result = await axios.get(completUrl);
+
+        const feed = result.data.feed;
+
+        console.log({
+          link: feed.link,
+          title: feed.title,
+          description: feed.description,
+        });
+
+        this.items = result.data.items;
+        this.isFetchingData = false;
+      } catch (err) {
+        console.log(err);
+        this.isFetchingData = false;
+      }
     },
   },
 });
